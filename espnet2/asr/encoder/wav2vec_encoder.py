f781efb9f5518357a2257ad07b5a7158026fa2bd
casssie-zhang <46862855+casssie-zhang@users.noreply.github.com>
2020-12-09 23:08:00 +0800

    debug print

--- espnet2/asr/encoder/wav2vec_encoder.py
+++ espnet2/asr/encoder/wav2vec_encoder.py
@@ -81,7 +81,7 @@ class Wav2vecTransformerEncoder(AbsEncoder):
             pass
 
         model.feature_grad_mult = 0  # zero grad
-        print("Conv feature extraction has been freezed")
+        print("Conv feature extraction has been freezed.")
 
 
         self.wav2vec = model
@@ -105,12 +105,14 @@ class Wav2vecTransformerEncoder(AbsEncoder):
         Returns:
             position embedded tensor and mask
         """
+        print(xs_pad.shape)
         self.wav2vec.feature_grad_mult = 0 # make sure conv feature extraction has been freezed
         xs_pad = self.wav2vec.forward(xs_pad, mask=False, features_only=True)['x']
         feats_lens = []
         for lens in ilens:
             feats_lens.append(get_output_lens(self.wav2vec.feature_extractor.conv_layers, lens))
         olens = torch.stack(feats_lens)
+        print(olens.shape)
 
         # xs_pad = self.projection(xs_pad)
 
